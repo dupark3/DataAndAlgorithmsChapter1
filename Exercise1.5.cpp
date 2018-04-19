3e2fc919b00800483c0b19445aa2191281ff9793
DuParkLinux <dupark3@gmail.com>
2018-04-19 07:17:03 -0400

    Strlen implemented

--- Exercise1.5.cpp
+++ Exercise1.5.cpp
@@ -1,8 +1,24 @@
 #include <cstddef> // size_t
+#include <iostream>
 #include <string>
 
 using namespace std;
 
+size_t strlen(char* ptr){
+    size_t size = 0;
+    while(*(ptr + size) != '\0')
+        ++size;
+    return size;
+}
+
+
+
 int main(){
+    char ptr1[] = "Hello";
+    char ptr2[] = "World";
+
+    // array to pointer decay
+    cout << strlen(ptr1) << '\n';
+
     return 0;
 }
