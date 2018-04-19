7a617aa51af76e48806b2dd30d78e37781fd9673
DuParkLinux <dupark3@gmail.com>
2018-04-19 07:24:12 -0400

    Strcmp implemented

--- Exercise1.5.cpp
+++ Exercise1.5.cpp
@@ -11,14 +11,34 @@ size_t strlen(char* ptr){
     return size;
 }
 
+bool strcmp(char* ptr1, char* ptr2){
+    // weed out words of different length
+    if (strlen(ptr1) != strlen(ptr2))
+        return false;
 
+    // if we make it here, only need to check for null char on one string, since they are of the same length 
+    for(size_t i = 0; *(ptr1 + i) != '\0'; ++i){
+        if (*(ptr1 + i) != *(ptr2 + i))
+            return false;
+    }
+
+    // if we make it here, every element in the null terminated char array is the same
+    return true;
+}
 
 int main(){
     char ptr1[] = "Hello";
     char ptr2[] = "World";
 
-    // array to pointer decay
-    cout << strlen(ptr1) << '\n';
+    // strlen test using array to pointer decay
+    cout << "Length of " << ptr1 << " is " << strlen(ptr1) << '\n';
+    cout << "Length of " << ptr2 << " is " << strlen(ptr2) << '\n';
+
+    // strcmp test
+    if (strcmp(ptr1, ptr2))
+        cout << ptr1 << " and " << ptr2 << " are the same" << '\n';
+    else
+        cout << ptr1 << " and " << ptr2 << " are different" << '\n';
 
     return 0;
 }
